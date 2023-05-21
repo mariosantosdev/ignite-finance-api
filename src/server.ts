ce847de02afbcfd5612fdeed5bc044f097493cc0
Mário Santos <mariodev7@gmail.com>
2023-05-21 19:28:46 -0300

    create simple api with fastify

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import fastify from 'fastify'
+
+const server = fastify()
+
+server.get('/', () => ({ hello: 'world' }))
+
+server
+    .listen({ port: 3333 })
+    .then(() => console.log('Server is running on port 3333'))
