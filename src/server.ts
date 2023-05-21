9d3ef678467b7fe46856067a8735435ed9124a42
Mário Santos <mariodev7@gmail.com>
2023-05-21 19:34:15 -0300

    setup eslint

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,5 @@ const server = fastify()
 server.get('/', () => ({ hello: 'world' }))
 
 server
-    .listen({ port: 3333 })
-    .then(() => console.log('Server is running on port 3333'))
+  .listen({ port: 3333 })
+  .then(() => console.log('Server is running on port 3333'))
